c5e6edf3cb38e736eaa8b67973fb04f22c1ecf13
Zach Lipton <zach@zachlipton.com>
2020-09-17 00:18:10 -0700

    add an extremely ugly slackbot to report diffs

--- compare.js
+++ compare.js
@@ -9,12 +9,17 @@ const ENDPOINTS = [
 ]
 
 const fetch = require('node-fetch');
-var changesets = require('diff-json');
+const changesets = require('diff-json');
 
-console.log('Running API comparison');
+let logOut = ""
+const log = function(msg) {
+  console.log(msg)
+  logOut += msg
+  logOut += "\n"
+}
 
 async function fetchEndpoint(url) {
-  console.log(`Fetching ${url}`)
+  log(`Fetching ${url}`)
   const response = await fetch(url);
   if (!response.ok) {
     throw new Error(`HTTP error! status: ${response.status}, url: ${url}`);
@@ -24,47 +29,59 @@ async function fetchEndpoint(url) {
   }
 }
 
-ENDPOINTS.forEach(endpoint => {
-  console.log("Comparing " + endpoint.name)
-  console.log("Ignoring intentional differences in fields: ", endpoint.ignorekeys)
-  const promises = [fetchEndpoint(endpoint.pubapi), fetchEndpoint(endpoint.internalapi)]
-  Promise.all(promises).then(results => {
-    const pubapi = {children: results[0]};
-    const internalapi = {children: results[1]};
-    const diffs = changesets.diff(pubapi, internalapi, {children: endpoint.embeddedkey});
+const runCompare = async function(callback) {
+  log('Running API comparison');
+  ENDPOINTS.forEach(endpoint => {
+    log("Comparing " + endpoint.name)
+    log("Ignoring intentional differences in fields: ", endpoint.ignorekeys)
+    const promises = [fetchEndpoint(endpoint.pubapi), fetchEndpoint(endpoint.internalapi)]
+    Promise.all(promises).then(results => {
+      const pubapi = {children: results[0]};
+      const internalapi = {children: results[1]};
+      const diffs = changesets.diff(pubapi, internalapi, {children: endpoint.embeddedkey});
 
-    if (diffs !== undefined && diffs.length > 0) {
-      diffs.forEach(diff => {
-        diff.changes.forEach((change, index) => {
-          change.changes = change.changes.filter(subChange => {
-            // filter out ignored keys
-            if (endpoint.ignorekeys.has(subChange.key)) {
-              return false
-            }
-            // filter out fields that are intentionally blank in the internal API and are empty strings
-            if (subChange.type == "remove" && subChange.value == "") {
-              return false
-            }
+      if (diffs !== undefined && diffs.length > 0) {
+        diffs.forEach(diff => {
+          diff.changes.forEach((change, index) => {
+            change.changes = change.changes.filter(subChange => {
+              // filter out ignored keys
+              if (endpoint.ignorekeys.has(subChange.key)) {
+                return false
+              }
+              // filter out fields that are intentionally blank in the internal API and are empty strings
+              if (subChange.type == "remove" && subChange.value == "") {
+                return false
+              }
 
-            // otherwise it's a real change. keep it
-            return true
+              // otherwise it's a real change. keep it
+              return true
+            })
+            if (change.changes.length == 0) {
+              diff.changes[index] = undefined
+            }
           })
-          if (change.changes.length == 0) {
-            diff.changes[index] = undefined
-          }
         })
-      })
-      diffs[0].changes = diffs[0].changes.filter(elem => {
-        return elem !== undefined
-      })
+        diffs[0].changes = diffs[0].changes.filter(elem => {
+          return elem !== undefined
+        })
 
-      if (diffs[0].changes.length > 0) {
-        console.log(`Found differences in ${endpoint.name}!!!`)
-        console.log(JSON.stringify(diffs[0].changes, null, 2))
-      } else {
-        console.log(`No differences for ${endpoint.name}`)
+        if (diffs[0].changes.length > 0) {
+          log(`Found differences in ${endpoint.name}!!!`)
+          log(JSON.stringify(diffs[0].changes, null, 2))
+        } else {
+          log(`No differences for ${endpoint.name}`)
+        }
       }
-    }
+      if (callback !== undefined) { callback(logOut) }
+    })
   })
-})
+}
 
+// standalone mode, run the thing
+if (!module.parent) {
+  runCompare()
+}
+
+module.exports = {
+    runCompare: runCompare,
+};
---
+++ diffbot.js
@@ -0,0 +1,99 @@
+const fetch = require('node-fetch');
+const { DateTime } = require('luxon');
+const metadataCompare = require('./compare.js')
+const { WebClient } = require('@slack/web-api');
+
+// compare state metadata
+metadataCompare.runCompare(metadataCompareResults => {
+  // compare states daily
+  let statesDailyOutput = "Comparing states daily: https://internal.covidtracking.com/compare\n"
+  fetchStatesDailyCompare().then(results =>{
+    let statesDailyCompareResults = Compare(results)
+    statesDailyOutput += `States daily comparison:  ${statesDailyCompareResults.length} differences found\n`
+
+    const output = metadataCompareResults + "\n" + statesDailyOutput;
+    postToSlack(output)
+  })
+})
+
+async function fetchStatesDailyCompare() {
+  let pubApi = await fetch("https://api.covidtracking.com/v1/states/daily.json")
+  if (!pubApi.ok) {
+    throw new Error(`HTTP error! status: ${pubApi.status}`);
+  }
+  let pubApiJson = await pubApi.json()
+  let internalApi = await fetch("https://internal.covidtracking.com/api-preview/states/daily")
+  if (!internalApi.ok) {
+    throw new Error(`HTTP error! status: ${internalApi.status}`);
+  }
+  let internalApiJson = await internalApi.json()
+  return {current: pubApiJson, preview: internalApiJson}
+}
+
+// copied straight out of https://github.com/COVID19Tracking/website-internal/blob/master/pages/compare.js
+const Compare = ({ preview, current }) => {
+  const results = []
+  const columns = [
+    {
+      title: 'Error',
+      dataIndex: 'error',
+      key: 'error',
+      width: 150,
+    },
+  ]
+
+  const dataSource = []
+  current.forEach((row) => {
+    const previewRow = preview.find(
+      (p) =>
+        p.state === row.state &&
+        DateTime.fromISO(p.date).equals(DateTime.fromISO(row.date)),
+    )
+    if (!previewRow) {
+      dataSource.push({
+        error: 'Does not exist in preview',
+        ...row,
+      })
+    } else {
+      const unmatchedFields = []
+      const fields = {}
+      const merged = {}
+      Object.keys(row).forEach((key) => {
+        merged[key] = `${row[key]} - ${previewRow[key]}`
+        if (key === 'date' || key === 'dateChecked' || key === 'lastUpdateEt') {
+          return
+        }
+        if (
+          !(row[key] === null && !previewRow[key]) &&
+          row[key] !== previewRow[key] &&
+          typeof previewRow[key] !== 'undefined'
+        ) {
+          unmatchedFields.push(key)
+        }
+      })
+      if (unmatchedFields.length) {
+        dataSource.push({
+          error: unmatchedFields.join(', '),
+          ...merged,
+        })
+      }
+    }
+  })
+  Object.keys(current[0]).forEach((key) => {
+    columns.push({
+      title: key,
+      dataIndex: key,
+      key,
+      width: 150,
+    })
+  })
+  return dataSource
+}
+
+function postToSlack(message) {
+  const web = new WebClient(process.env.SLACK_TOKEN);
+  web.chat.postMessage({
+    channel: process.env.SLACK_CHANNEL,
+    text: message
+  })
+}
